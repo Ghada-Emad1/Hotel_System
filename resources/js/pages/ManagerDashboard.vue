a232d794f130f8f58d3a5ba11576467b1ad04348
Ghada Emad <ghadaemad011@gmail.com>
2025-03-24 04:21:42 +0200

    route handle when you enter as admin

---
+++ resources/js/pages/ManagerDashboard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import AdminAppLayout from '@/layouts/AdminAppLayout.vue';
+import { Head, usePage } from '@inertiajs/vue3';
+import { Button } from '@/components/ui/button';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { computed } from 'vue';
+
+// Get the user permissions from the usePage hook
+const page = usePage();
+const user = computed(() => page.props.auth.user);
+console.log("User DAta",page.props.auth.user.roles);
+const can = computed(() => page.props.auth.user.permissions || {});
+
+
+// Title based on the user role
+const roleTitle = computed(() => {
+    if (user.value.role === 'admin') return 'Admin Dashboard';
+    if (user.value.role === 'manager') return 'Manager Dashboard';
+    if (user.value.role === 'receptionist') return 'Receptionist Dashboard';
+    return 'Dashboard';
+});
+</script>
+
+<template>
+
+    <Head :title="roleTitle" />
+
+    <AdminAppLayout>
+        
+    </AdminAppLayout>
+</template>
--- routes/web.php
+++ routes/web.php
@@ -11,13 +11,15 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth','verified'])->name('dashboard');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('admin/dashboard', function () {
         return Inertia::render('AdminDashboard', [
             'admin' => true,
         ]);
     })->name('admin.dashboard');
 });
+
+
 
 
 Route::prefix('admin/managers')->middleware(['auth', 'verified'])->group(function () {
