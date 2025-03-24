340b411f1eba231a2dd3ee2f115278057161db25
fatma311 <fatmamansour683@gmail.com>
2025-03-24 20:45:18 +0200

    manage manager

--- routes/web.php
+++ routes/web.php
@@ -1,42 +1,26 @@
 <?php
 
+use App\Http\Controllers\Admin\ManagerController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-use App\Http\Controllers\Admin\ManagerController;
-
-
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
-Route::get('/dashboard', function () {
+Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-
-Route::middleware(['auth', 'verified', 'role:admin'])->group(function () {
-    Route::get('/admin/dashboard', function () {
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('admin/dashboard', function () {
         return Inertia::render('AdminDashboard', [
             'admin' => true,
         ]);
     })->name('admin.dashboard');
-
-
-    Route::prefix('admin/managers')->name('managers.')->group(function () {
-        Route::get('/', [ManagerController::class, 'index'])->name('index');
-        Route::post('store', [ManagerController::class, 'store'])->name('store');
-        Route::put('{manager}/update', [ManagerController::class, 'update'])->name('update');
-        Route::delete('{manager}/destroy', [ManagerController::class, 'destroy'])->name('destroy');
-    });
 });
 
 
-<<<<<<< HEAD
-Route::middleware(['auth', 'verified', 'role:manager'])->group(function () {
-    Route::get('/manager/dashboard', function () {
-        return Inertia::render('AdminDashboard', [
-=======
 
 
 
@@ -53,20 +37,13 @@ Route::prefix('admin/managers')->middleware(['auth', 'verified'])->group(functio
 
 Route::middleware(['auth', 'role:manager'])->group(function () {
     Route::get('manager/dashboard', function () {
-        return Inertia::render('ManagerDashboard', [
->>>>>>> eb6e066b112f1d9f5b1d26efa15499058e5162b5
+        return Inertia::render('AdminDashboard', [
             'manager' => true,
         ]);
     })->name('manager.dashboard');
 });
 
-Route::middleware(['auth', 'role:receptionist'])->group(function () {
-    Route::get('receptionist/dashboard', function () {
-        return Inertia::render('ReceptionistDashboard', [
-            'receptionist' => true,
-        ]);
-    })->name('receptionist.dashboard');
-});
+
 
 require __DIR__ . '/settings.php';
 require __DIR__ . '/auth.php';
